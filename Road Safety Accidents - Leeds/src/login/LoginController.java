c35104de8c81ff89eea4dbbc19cf94568a70e6f8
perpetualLearner92 <c4.prathamesh.patil@gmail.com>
2019-03-17 17:47:22 +0000

    login screen changes

--- Road Safety Accidents - Leeds/src/login/LoginController.java
+++ Road Safety Accidents - Leeds/src/login/LoginController.java
@@ -8,13 +8,14 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import validation.RequiredField;
 
 /** Controls the login screen */
 public class LoginController {
   @FXML private TextField user;
-  @FXML private TextField password;
+  @FXML private PasswordField password;
   @FXML private Button loginButton;
   @FXML private Button guest;
   @FXML private RequiredField rqdUsername;
