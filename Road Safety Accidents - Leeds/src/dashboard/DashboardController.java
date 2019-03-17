acdf4a5da6134cae453c55aafe6d760478f5708a
perpetualLearner92 <c4.prathamesh.patil@gmail.com>
2019-03-17 16:20:52 +0000

    dashboard

--- Road Safety Accidents - Leeds/src/dashboard/DashboardController.java
+++ Road Safety Accidents - Leeds/src/dashboard/DashboardController.java
@@ -1,4 +1,32 @@
 package dashboard;
 
-public class DashboardController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.BarChart;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DashboardController  implements Initializable {
+
+    @FXML
+    private BarChart<String,Number> chart1;
+
+    @FXML
+    private BarChart<String,Number> chart2;
+
+    @FXML
+    private BarChart<String,Number> chart3;
+
+    @FXML
+    private BarChart<String,Number> chart4;
+    @Override // This method is called by the FXMLLoader when initialization is complete
+    public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
+        populateCharts();
+    }
+
+    private void populateCharts()
+    {
+
+    }
 }
