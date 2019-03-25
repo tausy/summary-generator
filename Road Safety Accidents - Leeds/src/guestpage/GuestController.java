f6b1461334424ebeb12c862611b3531bddeb2277
perpetualLearner92 <c4.prathamesh.patil@gmail.com>
2019-03-25 22:07:11 +0000

    guest validations

--- Road Safety Accidents - Leeds/src/guestpage/GuestController.java
+++ Road Safety Accidents - Leeds/src/guestpage/GuestController.java
@@ -2,14 +2,48 @@ package guestpage;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import landingpage.LoadApp;
 import masterpage.MasterScreenController;
+import validation.RequiredField;
 
-public class GuestController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GuestController  implements Initializable {
+
+    @FXML private TextField userName;
+    @FXML private TextField firstName;
+    @FXML private TextField guestNumber;
+    @FXML private TextField age;
+    @FXML private TextField gender;
+
+    @FXML private RequiredField rqdUsername;
+    @FXML private RequiredField rqdFirstName;
+    @FXML private RequiredField rqdAge;
+    @FXML private RequiredField rqdGender;
+
+
+    @Override // This method is called by the FXMLLoader when initialization is complete
+    public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
+        //TODO get from DB
+        guestNumber.setText("Guest Number ");
+    }
 
     @FXML
     public void openMasterPage()
+    {
+        if(validateLoginInput())
+        {
+            //TODO DB SAVE
+            loadMap();
+        }
+    }
+
+    private void loadMap()
     {
         try {
             LoadApp.stage.setHeight(700.0);
@@ -27,5 +61,24 @@ public class GuestController {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private boolean validateLoginInput()
+    {
+        boolean allGood=false;
+        rqdUsername.eval();
+        rqdFirstName.eval();
+        rqdGender.eval();
+        rqdAge.eval();
+        if (!(rqdUsername.hasErrorsProperty().get()||rqdFirstName.hasErrorsProperty().get()||rqdAge.hasErrorsProperty().get()
+        ||rqdGender.hasErrorsProperty().get()))
+        {
+            allGood=true;
+        }
+        else {
+            rqdUsername.eval();
+        }
+
+        return allGood;
     }
 }
