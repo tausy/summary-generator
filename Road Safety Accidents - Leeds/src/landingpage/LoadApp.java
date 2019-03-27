584483540de62933f106ab65d7705326987345cf
perpetualLearner92 <c4.prathamesh.patil@gmail.com>
2019-03-27 02:34:49 +0000

    comments

--- Road Safety Accidents - Leeds/src/landingpage/LoadApp.java
+++ Road Safety Accidents - Leeds/src/landingpage/LoadApp.java
@@ -12,6 +12,12 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * 1-Flow starts from here which invokes landing page
+ * 2-after landing page either login page flow is followed or registration or exit
+ * 3- after login or from guest page or registration page MASTER PAGE IS opened
+ * ON master page left side is menu and each menuitem is rendered on the same window.
+ */
 public class LoadApp extends Application {
 	public static Scene scene;
 	public static Stage stage;
